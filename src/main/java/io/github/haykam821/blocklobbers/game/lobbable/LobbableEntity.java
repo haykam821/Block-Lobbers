37b26579df9681faad9c8ffc0279426a690b5065
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-04-11 10:33:31 -0400

    Allow lobbables to apply knockback to entities

--- src/main/java/io/github/haykam821/blocklobbers/game/lobbable/LobbableEntity.java
+++ src/main/java/io/github/haykam821/blocklobbers/game/lobbable/LobbableEntity.java
@@ -1,18 +1,26 @@
 package io.github.haykam821.blocklobbers.game.lobbable;
 
+import java.util.Collections;
+
 import eu.pb4.polymer.api.entity.PolymerEntity;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.thrown.ThrownEntity;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldEvents;
 
 public class LobbableEntity extends ThrownEntity implements PolymerEntity {
+	public static final double KNOCKBACK_STRENGTH = 3;
+
 	private final Lobbable lobbable;
 
 	public LobbableEntity(EntityType<? extends LobbableEntity> entityType, World world, Lobbable lobbable) {
@@ -24,16 +32,34 @@ public class LobbableEntity extends ThrownEntity implements PolymerEntity {
 		this(entityType, world, new Lobbable(Blocks.AIR));
 	}
 
-	@Override
-	protected void onBlockHit(BlockHitResult hit) {
-		world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, new BlockPos(hit.getBlockPos()), this.lobbable.getRawId());
+	private void breakLobbable(BlockPos pos) {
+		world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, pos, this.lobbable.getRawId());
 		this.discard();
 	}
 
 	@Override
+	protected void onBlockHit(BlockHitResult hit) {
+		this.breakLobbable(hit.getBlockPos());
+	}
+
+	@Override
 	protected void onEntityHit(EntityHitResult hit) {
-		world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, new BlockPos(hit.getEntity().getEyePos()), this.lobbable.getRawId());
-		this.discard();
+		Entity entity = hit.getEntity();
+
+		Vec3d velocity = entity.getPos()
+			.subtract(this.getPos())
+			.normalize()
+			.add(0, 1, 0)
+			.multiply(KNOCKBACK_STRENGTH);
+
+		if (entity instanceof ServerPlayerEntity) {
+			ServerPlayerEntity player = (ServerPlayerEntity) entity;
+			player.networkHandler.sendPacket(new ExplosionS2CPacket(0, 0, 0, 0, Collections.emptyList(), velocity));
+		} else {
+			entity.setVelocity(velocity);
+		}
+
+		this.breakLobbable(new BlockPos(entity.getEyePos()));
 	}
 
 	@Override
